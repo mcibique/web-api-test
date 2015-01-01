042c869c920c7e1a8ca07bea2b5cc8777528026a
cibique <git.milos.cibulka@hotmail.com>
2015-01-01 20:35:41 +0000

    Initial Web API commit.

---
+++ Rimmon.WebApiTest/Application/WebApiTestApplication.cs
@@ -0,0 +1,27 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="WebApiTestApplication.cs" author="Rimmon">
+//      Copyright (c) Rimmon All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace Rimmon.WebApiTest
+{
+    using System.Web;
+    using System.Web.Http;
+
+    public class WebApiTestApplication : HttpApplication
+    {
+        #region Protected Methods
+
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(config =>
+            {
+                config.MapHttpAttributeRoutes();
+                config.Routes.MapHttpRoute(name: "DefaultApi", routeTemplate: "api/{controller}/{id}", defaults: new { id = RouteParameter.Optional });
+            });
+        }
+
+        #endregion
+    }
+}
---
+++ Rimmon.WebApiTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Inherits="Rimmon.WebApiTest.WebApiTestApplication" Language="C#" %>
