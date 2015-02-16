e084137da047067e52a78527339b60eb09e57204
mcibique <mcibique@hotmail.com>
2015-02-16 00:34:50 +0000

    Added credits information about web api.

---
+++ Rimmon.WebApiTest/Controllers/CreditsController.cs
@@ -0,0 +1,33 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="CreditsController.cs" author="Rimmon">
+//      Copyright (c) Rimmon All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace Rimmon.WebApiTest.Controllers
+{
+    using System.Web.Http;
+
+    [AllowAnonymous]
+    [RoutePrefix("api/credits")]
+    public class CreditsController : WebApiTestController
+    {
+        #region Public Methods
+
+        [HttpGet]
+        public IHttpActionResult Detail()
+        {
+            return
+                this.Ok(
+                    new
+                    {
+                        version = "1.0",
+                        author = "mcibique",
+                        sourceCode = "https://github.com/mcibique/web-api-test",
+                        gui = "https://github.com/mcibique/angular-test"
+                    });
+        }
+
+        #endregion
+    }
+}
